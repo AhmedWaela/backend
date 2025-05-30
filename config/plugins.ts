20326e41dc452528d31722ce690785ccddfe7b32
ahmedwael <ahmedwael99104@gmail.com>
2025-05-30 14:04:50 +0300

    third commit

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,15 @@
 export default () => ({});
+module.exports = ({ env }) => ({
+  'users-permissions': {
+    config: {
+      providers: {
+        facebook: {
+          enabled: true,
+          clientId: env('1375494060452682'),
+          clientSecret: env('0b490de1cca016a46dfaf5c154aa8316'),
+          callback: 'https://excellent-blessing-93fa28bbd4.strapiapp.com/api/auth/facebook/callback',
+        },
+      },
+    },
+  },
+});
