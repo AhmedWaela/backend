d0e124747f504ca23b7881cf91ed0fb53fbbc735
ahmedwael <ahmedwael99104@gmail.com>
2025-05-31 21:36:18 +0300

    configure twitter

--- config/plugins.ts
+++ config/plugins.ts
@@ -9,6 +9,11 @@ module.exports = ({ env }) => ({
           clientSecret: env('0b490de1cca016a46dfaf5c154aa8316'),
           callback: 'https://excellent-blessing-93fa28bbd4.strapiapp.com/api/auth/facebook/callback',
         },
+        twitter: {
+          key: env('a09tYmNWa1hjM1JuclZHbDBvRTk6MTpjaQ'),
+          secret: env('dRs33WfLBgN_oxqudZZm9ZFStisbMhgDBFjxvfR7DNwEinq4aq'),
+          callback: env('https://excellent-blessing-93fa28bbd4.strapiapp.com/api/auth/twitter/callback'),
+        },
       },
     },
   },
