42b0dc5cc594916f12b412e3265ad9e628c2a937
ahmedwael <ahmedwael99104@gmail.com>
2025-05-31 23:48:44 +0300

    a

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,19 +1,18 @@
-export default () => ({});
-module.exports = ({ env }) => ({
+module.exports = () => ({
   'users-permissions': {
     config: {
       providers: {
         facebook: {
           enabled: true,
-          clientId: env('1375494060452682'),
-          clientSecret: env('0b490de1cca016a46dfaf5c154aa8316'),
+          clientId: '1375494060452682',
+          clientSecret: '0b490de1cca016a46dfaf5c154aa8316',
           callback: 'https://excellent-blessing-93fa28bbd4.strapiapp.com/api/auth/facebook/callback',
         },
         twitter: {
           enabled: true,
-          key: env('a09tYmNWa1hjM1JuclZHbDBvRTk6MTpjaQ'),
-          secret: env('dRs33WfLBgN_oxqudZZm9ZFStisbMhgDBFjxvfR7DNwEinq4aq'),
-          callback: env('https://excellent-blessing-93fa28bbd4.strapiapp.com/api/auth/twitter/callback'),
+          key: '53gLeJKKqF0kTI6zcKmblYuVD',
+          secret: 'Y9co6HMubCef6NZVHPOnrlRSDvFkjPKc5cxFisRsJQW7pAP7zf',
+          callback: 'https://excellent-blessing-93fa28bbd4.strapiapp.com/api/auth/twitter/callback',
         },
       },
     },
